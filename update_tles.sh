65410689563ffc4848f06c818ce6a50e1ec79238
Samuel Gongora <s.gongoragarcia@gmail.com>
2014-10-11 07:16:14 +0200

    Now works with relative paths!

--- update_tles.sh
+++ update_tles.sh
@@ -1,127 +1,130 @@
 #!/bin/bash
 
-# TO-DO Mejorar la gestion de errores
+echo "Updating orbital elements!"
 
-echo "Descarga de los elementos orbitales"
+cd TLEs/
 
-rm ~/TLEs/*
+# Clean directory
+rm *
 
-wget -qr www.celestrak.com/NORAD/elements/amateur.txt -O ~/TLEs/amateur.txt
+wget -qr www.celestrak.com/NORAD/elements/amateur.txt -O amateur.txt
 echo "amateur.txt updated!     [1/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/beidou.txt -O ~/TLEs/beidou.txt
+wget -qr www.celestrak.com/NORAD/elements/beidou.txt -O beidou.txt
 echo "beidou.txt updated!      [2/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/cubesat.txt -O ~/TLEs/cubesat.txt
+wget -qr www.celestrak.com/NORAD/elements/cubesat.txt -O cubesat.txt
 echo "cubesat.txt updated!     [3/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/dmc.txt -O ~/TLEs/dmc.txt
+wget -qr www.celestrak.com/NORAD/elements/dmc.txt -O dmc.txt
 echo "dmc.txt updated!         [4/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/education.txt -O ~/TLEs/education.txt
+wget -qr www.celestrak.com/NORAD/elements/education.txt -O education.txt
 echo "education.txt updated!   [5/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/engineering.txt -O ~/TLEs/engineering.txt
+wget -qr www.celestrak.com/NORAD/elements/engineering.txt -O engineering.txt
 echo "engineering.txt updated! [6/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/galileo.txt -O ~/TLEs/galileo.txt
+wget -qr www.celestrak.com/NORAD/elements/galileo.txt -O galileo.txt
 echo "galileo.txt updated!     [7/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/geo.txt -O ~/TLEs/geo.txt
+wget -qr www.celestrak.com/NORAD/elements/geo.txt -O geo.txt
 echo "geo.txt updated!         [8/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/geodetic.txt -O ~/TLEs/geodetic.txt
+wget -qr www.celestrak.com/NORAD/elements/geodetic.txt -O geodetic.txt
 echo "geodetic.txt updated!    [9/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/glo-ops.txt -O ~/TLEs/glo-ops.txt
+wget -qr www.celestrak.com/NORAD/elements/glo-ops.txt -O glo-ops.txt
 echo "glo-ops.txt updated!    [10/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/globalstar.txt -O ~/TLEs/globalstar.txt
+wget -qr www.celestrak.com/NORAD/elements/globalstar.txt -O globalstar.txt
 echo "globalstar.txt updated! [11/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/gorizont.txt -O ~/TLEs/gorizont.txt
+wget -qr www.celestrak.com/NORAD/elements/gorizont.txt -O gorizont.txt
 echo "gorizont.txt updated!   [12/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/gps-ops.txt -O ~/TLEs/gps-ops.txt
+wget -qr www.celestrak.com/NORAD/elements/gps-ops.txt -O gps-ops.txt
 echo "gps-ops.txt updated!    [13/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/intelsat.txt -O ~/TLEs/intelsat.txt
+wget -qr www.celestrak.com/NORAD/elements/intelsat.txt -O intelsat.txt
 echo "intelsat.txt updated!   [14/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/iridium.txt -O ~/TLEs/iridium.txt
+wget -qr www.celestrak.com/NORAD/elements/iridium.txt -O iridium.txt
 echo "iridium.txt updated!    [15/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/military.txt -O ~/TLEs/military.txt
+wget -qr www.celestrak.com/NORAD/elements/military.txt -O military.txt
 echo "military.txt updated!   [16/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/molniya.txt -O ~/TLEs/molniya.txt
+wget -qr www.celestrak.com/NORAD/elements/molniya.txt -O molniya.txt
 echo "molniya.txt updated!    [17/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/musson.txt -O ~/TLEs/musson.txt
+wget -qr www.celestrak.com/NORAD/elements/musson.txt -O musson.txt
 echo "musson.txt updated!     [18/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/nnss.txt -O ~/TLEs/nnss.txt
+wget -qr www.celestrak.com/NORAD/elements/nnss.txt -O nnss.txt
 echo "nnss.txt updated!       [19/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/noaa.txt -O ~/TLEs/noaa.txt
+wget -qr www.celestrak.com/NORAD/elements/noaa.txt -O noaa.txt
 echo "noaa.txt updated!       [20/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/orbcomm.txt -O ~/TLEs/orbcomm.txt
+wget -qr www.celestrak.com/NORAD/elements/orbcomm.txt -O orbcomm.txt
 echo "orbcomm.txt updated!    [21/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/other-comm.txt -O ~/TLEs/other-comm.txt
+wget -qr www.celestrak.com/NORAD/elements/other-comm.txt -O other-comm.txt
 echo "other-comm.txt updated! [22/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/radar.txt -O ~/TLEs/radar.txt
+wget -qr www.celestrak.com/NORAD/elements/radar.txt -O radar.txt
 echo "radar.txt updated!      [23/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/raduga.txt -O ~/TLEs/raduga.txt
+wget -qr www.celestrak.com/NORAD/elements/raduga.txt -O raduga.txt
 echo "raduga.txt updated!     [24/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/resource.txt -O ~/TLEs/resource.txt
+wget -qr www.celestrak.com/NORAD/elements/resource.txt -O resource.txt
 echo "resource.txt updated!   [25/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/sarsat.txt -O ~/TLEs/sarsat.txt
+wget -qr www.celestrak.com/NORAD/elements/sarsat.txt -O sarsat.txt
 echo "sarsat.txt updated!     [26/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/sbas.txt -O ~/TLEs/sbas.txt
+wget -qr www.celestrak.com/NORAD/elements/sbas.txt -O sbas.txt
 echo "sbas.txt updated!       [27/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/science.txt -O ~/TLEs/science.txt
+wget -qr www.celestrak.com/NORAD/elements/science.txt -O science.txt
 echo "science.txt updated!    [28/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/tdrss.txt -O ~/TLEs/tdrss.txt
+wget -qr www.celestrak.com/NORAD/elements/tdrss.txt -O tdrss.txt
 echo "tdrss.txt updated!      [29/30]"
 sleep 0.3
 
-wget -qr www.celestrak.com/NORAD/elements/x-comm.txt -O ~/TLEs/x-comm.txt
+wget -qr www.celestrak.com/NORAD/elements/x-comm.txt -O x-comm.txt
 echo -e "x-comm.txt updated!     [30/30]\n"
 sleep 0.3
+
+cd ..
