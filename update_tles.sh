8e07d78af3f0a1e5273e26769be95919c0115c9a
Samuel Gongora <s.gongoragarcia@gmail.com>
2014-10-23 03:51:47 +0200

    Appearance improved.

--- update_tles.sh
+++ update_tles.sh
@@ -12,6 +12,23 @@ files=('amateur' 'beidou' 'cubesat' 'dmc' 'education' \
 'radar' 'raduga' 'resource' 'sarsat' 'sbas' 'science' \
 'tdrss' 'x-comm')
 
+bar=(
+'#                (4%)' '#                (7%)'
+'##              (10%)' '##              (13%)'
+'###             (17%)' '###             (20%)'   
+'####            (24%)' '####            (28%)'
+'#####           (31%)' '#####           (34%)'
+'######          (38%)' '######          (41%)'
+'#######         (44%)' '#######         (47%)'
+'#######         (50%)' '########        (54%)'
+'########        (57%)' '#########       (62%)'	
+'#########       (66%)' '##########      (70%)'
+'##########      (73%)' '###########     (77%)'
+'###########     (80%)' '############    (84%)'
+'############    (87%)' '#############   (90%)'
+'#############   (94%)' '##############  (97%)' 
+'############## (100%)')
+
 cd TLEs/
 
 n=1
@@ -23,7 +40,7 @@ do
 		# Remove file
     	rm $i
 	fi
-
+	
 	# Download new file
 	wget -qr www.celestrak.com/NORAD/elements/$i.txt -O $i.txt
 
@@ -31,7 +48,10 @@ do
 	if [[ -s $1 ]] ; then
 		echo "Error downloading $i.txt"
 	else
-		echo -e "Family [$n/29] updated!\t ${bold}$i${normal}"
+		printf ' %0.s' {0..70}
+		printf "\r"
+		echo -ne "Family [$n/29] updated!\t ${bar[$n]}\t ${bold}$i${normal}\r"
+		sleep 0.2
 	fi;
 	
 	n=$[$n + 1]
